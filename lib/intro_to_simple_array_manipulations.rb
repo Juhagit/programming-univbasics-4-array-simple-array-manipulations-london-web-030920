49ac058861872412cf41b6c5a5ca290a336be539
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-23 01:46:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,5 +12,17 @@ def using_pop(array)
 end 
 
 def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_unshift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+
   
   
