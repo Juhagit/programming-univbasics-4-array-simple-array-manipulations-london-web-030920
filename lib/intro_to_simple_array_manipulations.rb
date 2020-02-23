b94f6df43206cc096955296c4fb36a03b0346f90
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-23 01:36:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,7 @@
-#def using_push    
+def using_push    
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
     colors_in_the_rainbow.push("violet")
   
-#end 
+end 
 
 
