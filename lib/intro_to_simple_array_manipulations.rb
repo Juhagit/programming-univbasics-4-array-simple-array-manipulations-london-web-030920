8b665cf4f182b3dd1c53595bb4def31ab159b4a1
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-23 01:54:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,6 +23,6 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using.concat(my_favorite_things, all_my_favs)
+def using_concat(my_favorite_things, all_my_favs)
   all_my_favs.concat(my_favorite_things)
 end 
